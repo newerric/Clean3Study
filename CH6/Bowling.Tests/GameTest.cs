e9b6bdcc04cb2302b74e3393d48caeeed25c8eee
mikeceo <e_e1983@hotmail.com>
2018-08-05 01:42:03 +0800

    bowling game for page 80

--- CH6/Bowling.Tests/GameTest.cs
+++ CH6/Bowling.Tests/GameTest.cs
@@ -22,14 +22,14 @@ namespace Bowling.Tests
         #endregion
 
 
-        [TestMethod]
-        public void TestOneThrows()
-        {
-            game.Add(5);
+        // [TestMethod]
+        // public void TestOneThrows()
+        // {
+        //     game.Add(5);
 
-            Assert.AreEqual(5, game.Score);
-            Assert.AreEqual(1, game.CurrentFrame);
-        }
+        //     Assert.AreEqual(5, game.Score);
+        //     Assert.AreEqual(1, game.CurrentFrame);
+        // }
 
         [TestMethod]
         public void TestTwoThrowNoMark()
@@ -38,7 +38,7 @@ namespace Bowling.Tests
             game.Add(4);
 
             Assert.AreEqual(9, game.Score);
-            Assert.AreEqual(1, game.CurrentFrame);
+            Assert.AreEqual(2, game.CurrentFrame);
         }
 
         [TestMethod]
@@ -52,7 +52,7 @@ namespace Bowling.Tests
             Assert.AreEqual(18, game.Score);
             Assert.AreEqual(9, game.ScoreForFrame(1));
             Assert.AreEqual(18, game.ScoreForFrame(2));
-            Assert.AreEqual(2, game.CurrentFrame);
+            Assert.AreEqual(3, game.CurrentFrame);
         }
 
         [TestMethod]
@@ -63,6 +63,7 @@ namespace Bowling.Tests
             game.Add(3);
 
             Assert.AreEqual(13, game.ScoreForFrame(1));
+            Assert.AreEqual(2, game.CurrentFrame);
         }
 
         [TestMethod]
@@ -75,24 +76,49 @@ namespace Bowling.Tests
 
             Assert.AreEqual(13, game.ScoreForFrame(1));
             Assert.AreEqual(18, game.ScoreForFrame(2));
+            Assert.AreEqual(3, game.CurrentFrame);
         }
 
+        [TestMethod]
+        public void TestSimpleStrike()
+        {
+            game.Add(10);
+            game.Add(3);
+            game.Add(6);
 
+            Assert.AreEqual(19, game.ScoreForFrame(1));
+            Assert.AreEqual(28, game.Score);
+            Assert.AreEqual(3, game.CurrentFrame);
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Add(10);
+            }
+
+            Assert.AreEqual(300, game.Score);
+            Assert.AreEqual(11, game.CurrentFrame);
+        }
 
     }
 
     public class Game
     {
-        private int currentFrame;
+        private int currentFrame = 1;
         private bool isFirstThrow = true;
         private int score;
         private int[] throws = new int[21];
         private int currentThrow;
 
-
         public int Score
         {
-            get { return score; }
+            get
+            {
+                return ScoreForFrame(currentFrame - 1);
+            }
         }
 
         public int CurrentFrame
@@ -105,14 +131,31 @@ namespace Bowling.Tests
             throws[currentThrow++] = pins;
             score += pins;
 
+            AdjustCurrentFrame(pins);
+        }
+
+        private void AdjustCurrentFrame(int pins)
+        {
             if (isFirstThrow)
             {
-                isFirstThrow = false;
-                currentFrame++;
+                if (pins == 10) // Strike
+                {
+                    currentFrame++;
+                }
+                else
+                {
+                    isFirstThrow = false;
+                }
             }
             else
             {
                 isFirstThrow = true;
+                currentFrame++;
+            }
+
+            if (currentFrame > 11)
+            {
+                currentFrame = 11;
             }
         }
 
@@ -124,18 +167,25 @@ namespace Bowling.Tests
             for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
             {
                 int firstThrow = throws[ball++];
-                int secondThrow = throws[ball++];
-
-                int frameScore = firstThrow + secondThrow;
-
-                // spare
-                if (frameScore == 10)
+                if (firstThrow == 10)   // Strike
                 {
-                    score += frameScore + throws[ball];
+                    score += 10 + throws[ball] + throws[ball + 1];
                 }
                 else
                 {
-                    score += frameScore;
+                    int secondThrow = throws[ball++];
+                    int frameScore = firstThrow + secondThrow;
+
+                    // spare
+                    if (frameScore == 10)
+                    {
+                        score += frameScore + throws[ball];
+                    }
+                    else
+                    {
+                        score += frameScore;
+                    }
+
                 }
             }
 
