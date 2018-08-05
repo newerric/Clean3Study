05a62aa07ed53766bcdf19dbd19a8628f8d30274
mikeceo <e_e1983@hotmail.com>
2018-08-05 23:02:14 +0800

    [CH5] GeneratePrimes version 1

---
+++ CH5/Prime.Tests/GeneratePrimesTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prime
+{
+    [TestClass]
+    public class GeneratePrimesTest
+    {
+        [TestMethod]
+        public void TestPrimes()
+        {
+            int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+            Assert.AreEqual(nullArray.Length, 0);
+
+            int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+            Assert.AreEqual(minArray.Length, 1);
+            Assert.AreEqual(minArray[0], 2);
+
+            int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+            Assert.AreEqual(threeArray.Length, 2);
+            Assert.AreEqual(threeArray[0], 2);
+            Assert.AreEqual(threeArray[1], 3);
+
+            int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+            Assert.AreEqual(centArray.Length, 25);
+            Assert.AreEqual(centArray[24], 97);
+        }
+    }
+}
---
+++ CH5/Prime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
