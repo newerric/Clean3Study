46551ce01f4d56d0a9990ca6620bbc836cbf0f97
mikeceo <e_e1983@hotmail.com>
2018-08-05 15:39:16 +0800

    bowling game for page 92

--- CH6/Bowling.Service/Game.cs
+++ CH6/Bowling.Service/Game.cs
@@ -4,12 +4,10 @@ namespace Bowling.Service
 {
     public class Game
     {
-        private int ball;
+        private int score;
         private int currentFrame = 1;
         private bool isFirstThrow = true;
-        private int score;
-        private int[] throws = new int[21];
-        private int currentThrow;
+        private Scorer scorer = new Scorer();
 
         public int Score
         {
@@ -26,7 +24,7 @@ namespace Bowling.Service
 
         public void Add(int pins)
         {
-            throws[currentThrow++] = pins;
+            scorer.AddThrow(pins);
             score += pins;
 
             AdjustCurrentFrame(pins);
@@ -59,73 +57,8 @@ namespace Bowling.Service
 
         public int ScoreForFrame(int theFrame)
         {
-            ball = 0;
-            int score = 0;
-
-            for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
-            {
-                if (Strike())   // Strike
-                {
-                    score += 10 + NextTwoBallsForStrike;
-                    ball++;
-                }
-                else if (Spare())
-                {
-                    score += 10 + NextBallForSpare;
-                    ball += 2;
-                }
-                else
-                {
-                    score += HandleSecondThrow();
-                }
-            }
-
-            return score;
-        }
-
-        private int HandleSecondThrow()
-        {
-            int score = 0;
-
-            // spare
-            if (Spare())
-            {
-                ball += 2;
-                score += 10 + NextBallForSpare;
-            }
-            else
-            {
-                score += TwoBallsInFrame;
-                ball += 2;
-            }
-
-            return score;
-        }
-
-        private bool Strike()
-        {
-            return throws[ball] == 10;
-        }
-
-        private int NextTwoBallsForStrike
-        {
-            get { return (throws[ball + 1] + throws[ball + 2]); }
-        }
-
-        private bool Spare()
-        {
-            return throws[ball] + throws[ball + 1] == 10;
-        }
-
-        private int NextBallForSpare
-        {
-            get { return throws[ball + 2]; }
-        }
-
-        private int TwoBallsInFrame
-        {
-            get { return throws[ball] + throws[ball + 1]; }
-        }
+            return scorer.ScoreForFrame(theFrame);
+        }        
 
     }
 }
